9330591f0929abe6f55516b195e617100dd431b2
Sergei Sapyorov <skalolaz2012@yandex.ru>
2023-05-08 00:41:27 +0500

    bugfix/ автотесты 1.2.6

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ app.use((req, res, next) => {
 
   next();
 });
-app.use(function(req, res, next){
+app.use(function (req, res) {
   res.render('404', { status: 404, url: req.url });
 });
-app.use(function(err, req, res, next){
+app.use(function (err, res) {
   res.render('500', {
-      status: err.status || 500
-    , error: err
+    status: err.status || 500,
+    error: err,
   });
 });
 
