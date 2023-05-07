8bd52f1f862adc68ba6f997a68f8f4133dcdeee6
Sergei Sapyorov <skalolaz2012@yandex.ru>
2023-05-07 23:45:04 +0500

    bugfix/ автотесты 1.1.8

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
-const badRequest = () => {
-return next(res.status(500).send({ message: 'ошибка на сервере' }));
+const badRequest = (res) => {
+  return res.status(500).send({ message: 'ошибка на сервере' });
 };
 
 /* роутеры аддитивные, к тому, что написано в точке входа будут добавляться адреса-маршруты */
