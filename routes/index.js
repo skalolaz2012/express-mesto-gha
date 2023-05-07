457175d2ecc1f5c9458907ffc9adf345c6ec6d16
Sergei Sapyorov <skalolaz2012@yandex.ru>
2023-05-07 23:53:30 +0500

    bugfix/ автотесты 1.1.9

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
-const badRequest = (res) => {
-  return res.status(500).send({ message: 'ошибка на сервере' });
-};
-
 /* роутеры аддитивные, к тому, что написано в точке входа будут добавляться адреса-маршруты */
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-router.use('*', badRequest);
+router.use('*', () => {
+  return { message: 'ошибка на сервере' };
+});
 
 module.exports = router;
