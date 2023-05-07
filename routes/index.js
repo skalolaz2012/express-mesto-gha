86ef26833c90cedad7ba5c916f7ae7406e26eaa1
Sergei Sapyorov <skalolaz2012@yandex.ru>
2023-05-07 23:55:05 +0500

    bugfix/ автотесты 1.2.0

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,5 @@ const cardsRouter = require('./cards');
 /* роутеры аддитивные, к тому, что написано в точке входа будут добавляться адреса-маршруты */
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-router.use('*', () => {
-  return { message: 'ошибка на сервере' };
-});
 
 module.exports = router;
